5de2fdf71d3048678480856b6896a519b6171401
ANDRE MARTIN <48522092@ort>
2025-05-22 11:31:46 -0300

    localstorage arreglado

--- tp/src/App.jsx
+++ tp/src/App.jsx
@@ -1,27 +1,37 @@
-import { useState } from 'react'
-import './App.css'
-import Formulario from './components/Formulario'
-import ListadoReservas from './components/ListadoCitas'
+import { useState, useEffect } from 'react';
+import './App.css';
+import Formulario from './components/Formulario';
+import ListadoReservas from './components/ListadoCitas';
 
 function App() {
-  const eliminarReserva = (id) => {
-    setReservas(reservas.filter(reserva => reserva.id !== id))
-  }
+  // Leer reservas desde localStorage al iniciar
+  const [reservas, setReservas] = useState(() => {
+    const datosGuardados = localStorage.getItem('reservas');
+    return datosGuardados
+      ? JSON.parse(datosGuardados)
+      : [
+          {
+            id: 1,
+            equipo: 'Los Pibes del Fondo',
+            responsable: 'Tomi',
+            fecha: '2025-04-20',
+            hora: '22:00',
+            comentarios: 'Traer pelotas y pecheras',
+          },
+        ];
+  });
 
-  const [reservas, setReservas] = useState([
-    {
-      id: 1,
-      equipo: 'Los Pibes del Fondo',
-      responsable: 'Tomi',
-      fecha: '2025-04-20',
-      hora: '22:00',
-      comentarios: 'Traer pelotas y pecheras'
-    }
-  ])
+  useEffect(() => {
+    localStorage.setItem('reservas', JSON.stringify(reservas));
+  }, [reservas]);
 
   const agregarReserva = (reserva) => {
-    setReservas([...reservas, reserva])
-  }
+    setReservas([...reservas, reserva]);
+  };
+
+  const eliminarReserva = (id) => {
+    setReservas(reservas.filter((reserva) => reserva.id !== id));
+  };
 
   return (
     <div className="contenedor-app">
@@ -31,7 +41,7 @@ function App() {
         <ListadoReservas reservas={reservas} eliminarReserva={eliminarReserva} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
